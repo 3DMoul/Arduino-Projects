96e9290797ffdfdcc3874918df62ff663069ce1d
Svante <svanteman123@yahoo.se>
2025-12-01 10:20:20 +0100

    Add files via upload

---
+++ ThirdArduinoProject.ino
@@ -0,0 +1,56 @@
+const int  sensorPin = A0;
+const float baseLineTemp = 15.0;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  for(int pinNumber = 2; pinNumber < 5; pinNumber++)
+  {
+    pinMode(pinNumber, OUTPUT);
+    digitalWrite(pinNumber, LOW);
+  }
+}
+
+void loop() 
+{
+  int sensorVal = analogRead(sensorPin);
+
+  Serial.print("Sensor Value: ");
+  Serial.print(sensorVal);
+
+  float voltage = (sensorVal/1024.0) * 5.0;
+
+  Serial.print(", Volts: ");
+  Serial.print(voltage);
+
+  Serial.print(", degress C: ");
+  float temperature = (voltage - 0.5) * 100;
+  Serial.println(temperature);
+
+  if (temperature < baseLineTemp + 2)
+  {
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  }
+  else if (temperature >= baseLineTemp + 2 && temperature < baseLineTemp + 4)
+  {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  }
+  else if (temperature >= baseLineTemp + 4 && temperature < baseLineTemp + 6)
+  {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+  }
+  else if (temperature >= baseLineTemp + 6)
+  {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+  }
+  delay(1);
+}
